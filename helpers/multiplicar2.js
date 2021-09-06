1850a8cca4d15267ebe232fff97c54c93fe19c39
gabrielamarina <89638985+gabrielamarina@users.noreply.github.com>
2021-09-06 13:10:50 -0300

    Update multiplicar2.js

--- helpers/multiplicar2.js
+++ helpers/multiplicar2.js
@@ -1,49 +1,4 @@
 
-   
-   
-    /*const getSalario = (id)=> {
-    
-    return new Promise ((res, rej)=>{
-        const salario= salarios.find (s=> s.id=== id)?.salario;
-     
-        (salario)?
-        res (salario)
-        :rej ( `El Empleado con ${id} no tiene registrado su salario`)
-        
-    })*/
-// crear con promesa//
-/*const crearArchivo = (base = 1) => {
- try{  
-
-    return new Promise ((res, rej) => {
-    const fs = require ('fs');
-    
-    console.log (`tabla del ${base}`)    
-
-    let salida= ''; 
-    
-    for (let i=1; i<11; i++) {
-    
-    salida += `${base} x ${i} = ${base * i}\n`; 
-    }
-  
-    
-
-    fs.writeFileSync(`tabla-${base}.txt`, salida);
-    res (`tabla de ${base}.txt`);
-    
-    console.log (salida);
-});
-
-
-}
-catch(error) {throw (error)}
-}
-module.exports ={
-    crearArchivo
-}*/
-
-// crear con async
 const fs = require ('fs');
 const colors = require('colors')
 const crearArchivo = async (base=1, listar= false, hasta =10) => {
