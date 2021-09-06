66460ddcf7fc2d8203363260a4ab8a470dcce056
gabrielamarina <89638985+gabrielamarina@users.noreply.github.com>
2021-09-06 13:04:02 -0300

    Delete app3.js

--- app3.js
+++
@@ -1,5 +0,0 @@
-const base= 10 ;
-const {crearArchivo} = require('./helpers/multiplicar2');
-crearArchivo (base)
-.then (crearArchivo => console.log ('Archivo creado'))
-.catch (err => console.log (err))
